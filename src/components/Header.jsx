2d1c4585b28b342a1a8c2cf31acf7ea6d763855f
vyom10445 <vyom.chaturvedi10445knp@gmail.com>
2025-07-31 10:09:58 +0530

    improve header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,17 +3,20 @@ import React from "react";
 function Header() {
   return (
     <div className="header">
-      <div>
-        <img className="headerLogo" src="/Bite.png" alt="" />
+      <div className="header-logo-container">
+        {/* Assuming Bite.png is in the public folder, direct path is fine */}
+        <img className="headerLogo" src="/Bite.png" alt="Bite Rush Logo" />
       </div>
-      <div className="list">
+      <nav className="list">
+        {" "}
+        {/* Use <nav> for semantic navigation */}
         <ul className="unorderedList">
           <li className="listItem">Home</li>
           <li className="listItem">About</li>
           <li className="listItem">Contact Us</li>
           <li className="listItem">Cart</li>
         </ul>
-      </div>
+      </nav>
     </div>
   );
 }
