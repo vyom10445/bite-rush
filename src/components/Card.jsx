47ba8cb06daebe118f7d05b348297a24d36eedd8
vyom10445 <vyom.chaturvedi10445knp@gmail.com>
2025-07-31 15:32:02 +0530

    improve card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,11 +6,14 @@ function Card() {
       <img
         className="res-img"
         src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e0839ff574213e6f35b3899ebf1fc597"
-        alt=""
+        alt="Restaurant Image"
       />
-      <h3>Chinese Wok</h3>
-      <h5>Chinese , North Indian , Italian</h5>
-      <h5>4.1 stars</h5>
+      <div className="res-details">
+        <h3>Chinese Wok</h3>
+        <h5>Chinese , North Indian , Italian</h5>
+        <h5 className="rating">4.1 stars</h5>
+        <h5>38 Minutes</h5>
+      </div>
     </div>
   );
 }
