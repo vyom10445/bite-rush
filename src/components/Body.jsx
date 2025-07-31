1e5780db3d073cc3205083ed698e77763a620e46
vyom10445 <vyom.chaturvedi10445knp@gmail.com>
2025-07-31 15:09:54 +0530

    add search and card

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Card from "./Card";
 
 function Body() {
-  return <div>Body</div>;
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="resContainer">
+        <Card />
+      </div>
+    </div>
+  );
 }
 
 export default Body;
---
+++ src/components/Card.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Card() {
+  return (
+    <div className="res-card">
+      <img
+        className="res-img"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e0839ff574213e6f35b3899ebf1fc597"
+        alt=""
+      />
+      <h3>Chinese Wok</h3>
+      <h5>Chinese , North Indian , Italian</h5>
+      <h5>4.1 stars</h5>
+    </div>
+  );
+}
+
+export default Card;
