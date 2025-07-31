5d76b03000c78da7f99843a981fb580c3b0219c9
vyom10445 <vyom.chaturvedi10445knp@gmail.com>
2025-08-01 01:56:27 +0530

    add swiggy data and dynamic cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 
-function Card() {
+function Card({ resData }) {
   return (
     <div className="res-card">
       <img
         className="res-img"
-        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e0839ff574213e6f35b3899ebf1fc597"
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+          resData.info.cloudinaryImageId
+        }
         alt="Restaurant Image"
       />
       <div className="res-details">
-        <h3>Chinese Wok</h3>
-        <h5>Chinese , North Indian , Italian</h5>
-        <h5 className="rating">4.1 stars</h5>
-        <h5>38 Minutes</h5>
+        <h3>{resData.info.name}</h3>
+        <h5>{resData.info.cuisines.join(", ")}</h5>
+        <h5 className="rating">{resData.info.avgRating} stars</h5>
+        <h5>{resData.info.costForTwo}</h5>
+        <h5>{resData.info.sla.deliveryTime} Mins</h5>
       </div>
     </div>
   );
