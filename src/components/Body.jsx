987eed81289667304dba12f640889a548f31506b
vyom10445 <vyom.chaturvedi10445knp@gmail.com>
2025-08-08 23:45:00 +0530

    get swiggy api data

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import Card from "./Card";
 import resList from "../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function Body() {
   const [restaurantList, setRestaurantList] = useState(resList);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.46310&lng=80.34790&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(json);
+  };
+
   return (
     <div className="body">
       <div
