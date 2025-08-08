a78c45fdba292dfb4b176b0f927f6cfe46dd849b
vyom10445 <vyom.chaturvedi10445knp@gmail.com>
2025-08-08 18:57:12 +0530

    add topRated btn design and feature issue

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import Card from "./Card";
 import resList from "../utils/mockData";
+import { onState } from "react";
+
+const [restaurantList, setRestaurantList] = onState(resList);
 
 function Body() {
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div
+        className="filter-btn"
+        onClick={() => {
+          const filteredList = resList.map((res) => res.info.avgRating > 4);
+          setRestaurantList(filteredList);
+        }}
+      >
+        Filter Top Restaurants
+      </div>
       <div className="resContainer">
         {resList.map((restaurant) => (
           <Card key={restaurant.info.id} resData={restaurant} />
