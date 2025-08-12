f74c209b2e7a0b8383f59f1e6f70c6c516cb1ec2
vyom10445 <vyom.chaturvedi10445knp@gmail.com>
2025-08-12 19:55:33 +0530

    query for router

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,19 @@ import { useState } from "react";
 
 import Header from "./components/Header";
 import Body from "./components/Body";
+import About from "./components/About";
+import { createBrowserRouter } from "react-router-dom";
+
+const AppRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+]);
 
 function App() {
   return (
---
+++ src/components/About.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="about-container">
+      <div className="about-content">
+        <h1 className="about-title">About Bite Rush</h1>
+        <p className="about-description">
+          Bite Rush is your one-stop solution for ordering delicious food from
+          your favorite local restaurants. Our mission is to connect you with a
+          wide variety of cuisines and ensure a fast, reliable delivery
+          experience.
+        </p>
+        <p className="about-description">
+          We believe that good food brings people together. With a user-friendly
+          interface and a diverse selection of restaurants, we strive to make
+          every meal a memorable one. Thank you for choosing Bite Rush!
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
